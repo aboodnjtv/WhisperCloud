f1795d80a19555f41237185723c347387ee8a23b
Mansi-9542 <mansirahul.patel@sjsu.edu>
2025-10-23 13:02:24 -0700

    Implement admin and API page messages display

--- backend/app.js
+++ backend/app.js
@@ -18,13 +18,25 @@ app.set("views",path.join(__dirname,"../frontend/views"));
 app.use(express.static("../frontend/public"));
 app.use(express.urlencoded({ extended: true }));
 
-app.use(session({ secret: process.env.SESSION_SECRET }));
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_URI
+    }),
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 2
+    }
+}));
 
 // import routes
 // user routes 
 const userRoutes = require("./routes/userRoutes");
+const messageRoutes = require("./routes/messageRoutes");
 
 app.use("/", userRoutes);
+app.use("/", messageRoutes);
 
 
 const port = 3000;
